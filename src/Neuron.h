a9aef323cfb1a901ba7a7d788cb07a7e2cd1cc6f
Max Kostyukov <maximkostyukov@yandex.ru>
2022-09-19 03:01:23 +0300

    Added clang compatibility

--- src/Neuron.h
+++ src/Neuron.h
@@ -13,7 +13,7 @@ private:
 	double phase;
 	double w;
 public:
-	Neuron(double phase_start = 0.0, double param = 1);
+	Neuron(double = 0.0, double = 1);
 	double getPhase();
 	void setPhase(double setedPhase);
 	double getParam();
